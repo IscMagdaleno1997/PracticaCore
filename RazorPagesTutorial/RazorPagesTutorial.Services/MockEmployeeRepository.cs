13c186fda46fd35c1e0814cf85d716384f777af8
Magdaleno <iscmagaleno@gmail.com>
2020-01-15 22:11:07 -0600

    Commit inicial del proyecto practica

---
+++ RazorPagesTutorial/RazorPagesTutorial.Services/MockEmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using RazorPagesTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RazorPagesTutorial.Services
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){ Id = 1, Name = "Mary", Departament = Dept.HR, Email = "a@a.com", Photopath="LogoBlanco.png" },
+                new Employee(){ Id = 1, Name = "John", Departament = Dept.HR, Email = "b@b.com", Photopath="LogoBlanco2.png" },
+                new Employee(){ Id = 1, Name = "Jane", Departament = Dept.HR, Email = "c@c.com", Photopath="LogoN-Edit.png" },
+                new Employee(){ Id = 1, Name = "Peter", Departament = Dept.HR, Email = "e@e.com", Photopath="LogoRolando.png" }
+
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeList;
+        }
+    }
+}
---
+++ RazorPagesTutorial/RazorPagesTutorial/Pages/Employees/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesTutorial.Models;
+using RazorPagesTutorial.Services;
+
+namespace RazorPagesTutorial
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly IEmployeeRepository employeeRepository;
+
+        public IEnumerable<Employee> Employees { get; set; }
+
+        public IndexModel( IEmployeeRepository employeeReposity )
+        {
+            this.employeeRepository = employeeReposity;
+        }
+
+
+        public void OnGet()
+        {
+            Employees = employeeRepository.GetAllEmployees();
+        }
+    }
+}
